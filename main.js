99e5717ef11c1b9f479e05de8759d467e2fc1e6b
FrejNilsson <frejnilsson@gmail.com>
2017-03-09 21:19:38 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+    var links = [{
+        name: 'nilsson',
+        link: 'http://steamcommunity.com/profiles/76561198064129752/'
+    }, {
+        name: 'concreteproductions',
+        link: 'http://steamcommunity.com/id/Emelge'
+    }, {
+        name: 'frost',
+        link: '#'
+    }, {
+        name: 'rubin',
+        link: '#'
+    }, {
+        name: 'axel',
+        link: 'http://steamcommunity.com/profiles/76561198079922052'
+    },{
+        name: 'ludwig',
+        link: 'http://steamcommunity.com/id/TomzBroz/'
+    }, ];
+
+    for (var i in links) {
+        var link = links[i];
+
+        $('#marquee').append('<a href="' + link.link + '">' + link.name + '</a>');
+
+        var link = $('#marquee').children('a').last();
+
+        link.mouseover(function() {
+            $('#hover')[0].play();
+        });
+
+        link.mousedown(function() {
+            $('#click')[0].play();
+        });
+
+        link.mouseup(function() {
+            $('#release')[0].play();
+        });
+
+        if (i != links.length - 1) {
+            $('#marquee').append(' - ');
+        }
+    }
+
+    $('#marquee').marquee({
+        duration: 13000,
+        gap: 400,
+        delayBeforeStart: 0,
+        direction: 'left',
+        duplicated: true
+    });
+});
